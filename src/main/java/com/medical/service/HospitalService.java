1b2c660a2fe9086d476c8c816c0617beb9ff6747
Liyikun123 <111033711+Liyikun123@users.noreply.github.com>
2022-08-18 11:00:31 +0800

    Update HospitalService.java

--- src/main/java/com/medical/service/HospitalService.java
+++ src/main/java/com/medical/service/HospitalService.java
@@ -1,16 +1,47 @@
-package com.medical.service;
+package com.medical.service.impl;
 
 import com.medical.entity.Hospital;
-import com.baomidou.mybatisplus.extension.service.IService;
+import com.medical.mapper.HospitalMapper;
+import com.medical.service.HospitalService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * <p>
- * 医院 服务类
+ * 医院 服务实现类
  * </p>
  *
  * @author JiaJieTang
  * @since 2022-08-11
  */
-public interface HospitalService extends IService<Hospital> {
+@Service
+public class HospitalServiceImpl extends ServiceImpl<HospitalMapper, Hospital> implements HospitalService {
+  @Resource
+  HospitalMapper hospitalMapper;
+
+  @Override
+  public boolean addHospital(Hospital hospital) {
+    return hospitalMapper.add(hospital)>0;
+  }
+
+  @Override
+  public boolean deleteHospital(int id) {
+    return hospitalMapper.delete(id)>0;
+  }
+
+  @Override
+  public boolean updateHospital(Hospital hospital) {
+    return hospitalMapper.update(hospital)>0;
+  }
+
+  @Override
+  public List<Hospital> likeName(String hName) {
+    return hospitalMapper.Likes(hName);
+  }
+
 
 }
