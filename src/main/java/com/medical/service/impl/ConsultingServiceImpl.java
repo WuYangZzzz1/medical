0c7ba6277b1bc7f17606d9dcb909a8c969e63237
Liyikun123 <111033711+Liyikun123@users.noreply.github.com>
2022-08-19 15:13:22 +0800

    Update ConsultingServiceImpl.java

--- src/main/java/com/medical/service/impl/ConsultingServiceImpl.java
+++ src/main/java/com/medical/service/impl/ConsultingServiceImpl.java
@@ -1,5 +1,7 @@
 package com.medical.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.medical.entity.Consulting;
 import com.medical.mapper.ConsultingMapper;
 import com.medical.service.ConsultingService;
@@ -16,5 +18,16 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class ConsultingServiceImpl extends ServiceImpl<ConsultingMapper, Consulting> implements ConsultingService {
-
+    public Boolean update(Consulting consulting){
+        UpdateWrapper<Consulting> wrapper =new UpdateWrapper<>();
+        wrapper.eq("id",consulting.getId()).set("describle",consulting.getDescrible()).set("c_state",consulting.getCState());
+        int update = baseMapper.update(consulting, wrapper);
+        return update>0;
+    }
+    public Consulting findById(Integer id){
+        QueryWrapper<Consulting> wrapper = new QueryWrapper<>();
+        wrapper.eq("id",id).select("c_state","doid");
+        Consulting consulting = baseMapper.selectOne(wrapper);
+        return consulting;
+    }
 }
