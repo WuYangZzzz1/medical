46690431799ee74a28714e9776e75e1ac985c3c2
Liyikun123 <111033711+Liyikun123@users.noreply.github.com>
2022-08-19 15:12:34 +0800

    Update Vaccine.java

--- src/main/java/com/medical/entity/Vaccine.java
+++ src/main/java/com/medical/entity/Vaccine.java
@@ -21,6 +21,8 @@ package com.medical.entity;
     public class Vaccine implements Serializable {
 
     private static final long serialVersionUID = 1L;
+        
+    private int id;
 
             /**
             * 疫苗名称
