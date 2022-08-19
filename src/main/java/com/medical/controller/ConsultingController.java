07ed11de6501025955921f695bcf8ba9a83c484d
Liyikun123 <111033711+Liyikun123@users.noreply.github.com>
2022-08-19 15:10:30 +0800

    Update ConsultingController.java

--- src/main/java/com/medical/controller/ConsultingController.java
+++ src/main/java/com/medical/controller/ConsultingController.java
@@ -5,11 +5,10 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.medical.entity.*;
 import com.medical.service.impl.ConsultingServiceImpl;
 import com.medical.service.impl.DoctorServiceImpl;
-import com.medical.service.impl.UserServiceImpl;
 import com.medical.util.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -33,8 +32,7 @@ import java.util.Map;
 public class ConsultingController {
     @Autowired
     private ConsultingServiceImpl consultingServiceImpl;
-    @Autowired
-    private UserServiceImpl userServiceImpl;
+
     @Autowired
     private DoctorServiceImpl doctorServiceImpl;
 
@@ -56,8 +54,6 @@ public class ConsultingController {
         QueryWrapper<User> userQueryWrapper=new QueryWrapper<>();
         userQueryWrapper.eq("id",user.getId());
 
-
-
         for (Consulting consulting:consultings){
             if (consulting.getUId()==user.getId()) {
                 // TODO: 2022/8/16 获取医生信息
@@ -73,4 +69,39 @@ public class ConsultingController {
         return Result.success(myconsulting);
     }
 
+    /**
+     * 修改（添加）医嘱
+     * @param session
+     * @param id
+     * @param describle
+     * @return
+     */
+    @GetMapping("/describle/{id}")
+    public Object jieDa(HttpSession session, @PathVariable Integer id, String describle){
+
+        Map<String,Object> map = new HashMap<>();
+        User user = (User) session.getAttribute("user");
+        if (user==null){
+            map.put("data","请先登录");
+            return map;
+        }
+        Consulting byId = consultingServiceImpl.findById(id);
+
+        if (!user.getDoid().equals(byId.getDoid())){
+            map.put("data","该用户不是医生或不是对应的医生,不能解答");
+            return map;
+        }else {
+            if (byId.getCState().equals("未解答")){
+                byId.setId(id);
+                byId.setCState("已解答");
+                byId.setDescrible(describle);
+                Boolean update = consultingServiceImpl.update(byId);
+                map.put("data","解答成功");
+            }
+            else {
+                map.put("data","咨询状态错误");
+            }
+        }
+        return map;
+    }
 }
